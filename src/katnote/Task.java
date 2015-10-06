000c027003d9ff0e43a5da006433d76a1ef8490b
skylevis <skylavis@hotmail.com>
2015-10-07 01:33:29 +0800

    Changed the Task Class and Added basic functionality to the Model and Storage components
    The task class will still need another constructor that uses
    CommandDetail as the argument.
    Model does not support redo/undo functions yet.
    For modfiy(), Logic will assume Parser knows what is changed and pass
    the details of the changes as a commandDetail for creating a Task Object
    which will have <null> fields for fields to remain unchanged and
    non-null for otherwise. Note <null> != <emptyString>

--- src/katnote/Task.java
+++ src/katnote/Task.java
@@ -3,6 +3,8 @@ package katnote;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.json.simple.JSONArray;
+
 /**
  * This will be the association class used across KatNote. It will be the standardized task object that will be passed between components.
  * @author sk
@@ -23,29 +25,50 @@ public class Task {
     private Boolean _completed;
     
     // Constants
+    private static final int MAX_ARG_SIZE = 10;
+    
+    private static final int INDEX_ID = 0;
+    private static final int INDEX_TITLE = 1;
+    private static final int INDEX_TASK_TYPE = 2;
+    private static final int INDEX_START_DATE = 3;
+    private static final int INDEX_END_DATE = 4;
+    private static final int INDEX_REPEAT_OPTION = 5;
+    private static final int INDEX_TERMINATE_DATE = 6;
+    private static final int INDEX_DESCRIPTION = 7;
+    private static final int INDEX_CATEGORY = 8;
+    private static final int INDEX_COMPLETED = 9;
+    
+    // Messages
     private static final String MSG_ERR_PARSE_EXCEPTION = "Error: Unable to parse inputs to Task object. ";
-    private static final int MAX_ARG = 10;
+    
+    
+    // Format
     private static final SimpleDateFormat DATE_FORMAT_LONG = new SimpleDateFormat("EEEE, MMM dd, yyyy HH:mm:ss a"); // Eg: "Friday, Jun 7, 2013 12:10:56 PM"
     private static final SimpleDateFormat DATE_FORMAT_DAY = new SimpleDateFormat("E, MMM dd yyyy"); // Eg: "Fri, June 7 2013"
     private static final SimpleDateFormat DATE_FORMAT_SHORT = new SimpleDateFormat("MMM dd, yyyy"); // Eg: "Jun 7, 2013"
 
-    // Constructor
-    public Task(String[] args) throws Exception {
+    
+    // Constructor using JSONArray
+    public Task(JSONArray array) throws Exception {
         
-        assert(args.length == MAX_ARG);
+        assert(array.size() == MAX_ARG_SIZE);
         
+        String[] args = new String[MAX_ARG_SIZE];
+        for (int i=0; i<MAX_ARG_SIZE; i++) {
+            args[i] = (String) array.get(i);
+        }
         // Currently choosing DATE_FORMAT_LONG for all dates.
         try {
-            set_id(Integer.parseInt(args[0]));
-            set_title(args[1]);
-            set_taskType(args[2]);
-            set_startDate(DATE_FORMAT_LONG.parse(args[3]));
-            set_endDate(DATE_FORMAT_LONG.parse(args[4]));
-            set_repeatOption(args[5]);
-            set_terminateDate(DATE_FORMAT_LONG.parse(args[6]));
-            set_description(args[7]);
-            set_category(args[8]);
-            set_completed(Boolean.parseBoolean(args[9]));
+            setID(Integer.parseInt(args[INDEX_ID]));
+            setTitle(args[INDEX_TITLE]);
+            setTaskType(args[INDEX_TASK_TYPE]);
+            setStartDate(DATE_FORMAT_LONG.parse(args[INDEX_START_DATE]));
+            setEndDate(DATE_FORMAT_LONG.parse(args[INDEX_END_DATE]));
+            setRepeatOption(args[INDEX_REPEAT_OPTION]);
+            setTerminateDate(DATE_FORMAT_LONG.parse(args[INDEX_TERMINATE_DATE]));
+            setDescription(args[INDEX_DESCRIPTION]);
+            setCategory(args[INDEX_CATEGORY]);
+            setCompleted(Boolean.parseBoolean(args[INDEX_COMPLETED]));
         } catch (Exception e) {
             throw new Exception(MSG_ERR_PARSE_EXCEPTION + e);
         }
@@ -53,83 +76,83 @@ public class Task {
 
     // Getter and Setter
     
-    public Integer get_id() {
+    public Integer getID() {
         return _id;
     }
 
-    public void set_id(Integer _id) {
+    public void setID(Integer _id) {
         this._id = _id;
     }
 
-    public String get_title() {
+    public String getTitle() {
         return _title;
     }
 
-    public void set_title(String _title) {
+    public void setTitle(String _title) {
         this._title = _title;
     }
 
-    public String get_taskType() {
+    public String getTaskType() {
         return _taskType;
     }
 
-    public void set_taskType(String _taskType) {
+    public void setTaskType(String _taskType) {
         this._taskType = _taskType;
     }
 
-    public Date get_startDate() {
+    public Date getStartDate() {
         return _startDate;
     }
 
-    public void set_startDate(Date _startDate) {
+    public void setStartDate(Date _startDate) {
         this._startDate = _startDate;
     }
 
-    public Date get_endDate() {
+    public Date getEndDate() {
         return _endDate;
     }
 
-    public void set_endDate(Date _endDate) {
+    public void setEndDate(Date _endDate) {
         this._endDate = _endDate;
     }
 
-    public String get_repeatOption() {
+    public String getRepeatOption() {
         return _repeatOption;
     }
 
-    public void set_repeatOption(String _repeatOption) {
+    public void setRepeatOption(String _repeatOption) {
         this._repeatOption = _repeatOption;
     }
 
-    public Date get_terminateDate() {
+    public Date getTerminateDate() {
         return _terminateDate;
     }
 
-    public void set_terminateDate(Date _terminateDate) {
+    public void setTerminateDate(Date _terminateDate) {
         this._terminateDate = _terminateDate;
     }
 
-    public String get_description() {
+    public String getDescription() {
         return _description;
     }
 
-    public void set_description(String _description) {
+    public void setDescription(String _description) {
         this._description = _description;
     }
 
-    public String get_category() {
+    public String getCategory() {
         return _category;
     }
 
-    public void set_category(String _category) {
+    public void setCategory(String _category) {
         this._category = _category;
     }
 
-    public Boolean get_completed() {
+    public Boolean getCompleted() {
         return _completed;
     }
 
-    public void set_completed(Boolean _completed) {
+    public void setCompleted(Boolean _completed) {
         this._completed = _completed;
     }
     
