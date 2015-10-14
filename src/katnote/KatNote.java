d9240fe2aa1b3a225db580b6d876315534a95fbd
wei zheng <wzchua77@gmail.com>
2015-10-14 19:50:27 +0800

    Basic UI for commandLineInterface for normal tasks

--- src/katnote/KatNote.java
+++ src/katnote/KatNote.java
@@ -6,7 +6,7 @@ import katnote.ui.GraphicalUserInterface;
 public class KatNote {
 
     public static void main(String[] args) {
-        if (args.length != 0 && args[0] == "cli") {
+        if (args.length != 0 && args[0].equals("cli")) {
             CommandLineInterface cli = new CommandLineInterface();
         } else {
             GraphicalUserInterface.launch(GraphicalUserInterface.class, args);
