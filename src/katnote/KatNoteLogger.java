eed119f3632c24597791637f659ad7567cfd8ee6
wei zheng <wzchua77@gmail.com>
2015-10-22 19:54:04 +0800

    fixed file path interaction bug with jar build

--- src/katnote/KatNoteLogger.java
+++ src/katnote/KatNoteLogger.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -23,10 +24,16 @@ public class KatNoteLogger {
     private KatNoteLogger() {        
         try {
             File logFile = new File(LOGGING_PROPERTIES_FILENAME);
-            String url = getClass().getResource(DEFAULT_LOGGING_PROPERTIES_FILENAME).getPath();
-            File defaultLogFile = new File(url);
             if(!logFile.exists()){
-                Files.copy(defaultLogFile.toPath(), new FileOutputStream(logFile));
+                InputStream defaultFile = getClass().getResourceAsStream(DEFAULT_LOGGING_PROPERTIES_FILENAME);
+                FileOutputStream outStream = new FileOutputStream(logFile);
+                int readBytes;
+                byte[] buffer = new byte[4096];
+                while ((readBytes = defaultFile.read(buffer)) > 0) {
+                    outStream.write(buffer, 0, readBytes);
+                }
+                defaultFile.close();
+                outStream.close();
             }
             logManager.readConfiguration(new FileInputStream(logFile));
             fh = new FileHandler(KATNOTE_LOG_FILENAME, false);
