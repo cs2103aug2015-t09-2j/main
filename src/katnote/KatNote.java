6e0c1948ee243e9cdd3c64f05eefa2baf5496ccd
wei zheng <wzchua77@gmail.com>
2015-11-01 17:02:57 +0800

    Console-based support dropped

--- src/katnote/KatNote.java
+++ src/katnote/KatNote.java
@@ -1,15 +1,10 @@
 package katnote;
 
-import katnote.ui.CommandLineInterface;
 import katnote.ui.GraphicalUserInterface;
 
 public class KatNote {
 
     public static void main(String[] args) {
-        if (args.length != 0 && args[0].equals("cli")) {
-            CommandLineInterface cli = new CommandLineInterface();
-        } else {
             GraphicalUserInterface.launch(GraphicalUserInterface.class, args);
-        }
     }
 }
